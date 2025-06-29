93a53abb8ef43ec4063e57a934e5483a2ebbabbe
lj47312 <30307322+lj47312@users.noreply.github.com>
2025-06-29 23:50:15 +0800

    Update frps-install.sh

--- frps-install.sh
+++ frps-install.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 ###export###
 export PATH
-export FRPS_VER=0.53.2
+export FRPS_VER=0.63.0
 export FRPS_INIT="https://raw.githubusercontent.com/lj47312/frp/main/frps.init"
 export aliyun_download_url="https://gitee.com/lj47312/frp/releases/download"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
